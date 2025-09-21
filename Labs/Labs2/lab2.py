3111c40fb07aa79f00b1cf47ae55efd08c1da01b
Jack-Hammarberg <Jackhammarberg@gmail.com>
2025-09-21 21:49:12 +0200

    Change from absolute path to relative path

--- Labs/Labs2/lab2.py
+++ Labs/Labs2/lab2.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 # https://www.w3schools.com/python/pandas/pandas_csv.asp 
-trainingSet = pd.read_csv(r"C:\Users\Jack\python-programming-Jack-Hammarberg\Labs\Labs2\Lab2-data-and-testpoints\datapoints.csv")
-testSet = pd.read_csv(r"C:\Users\Jack\python-programming-Jack-Hammarberg\Labs\Labs2\Lab2-data-and-testpoints\testpoints.csv")
+currDir = os.path.abspath(os.path.dirname(__file__))
+trainingSet = pd.read_csv(f"{currDir}/Lab2-data-and-testpoints/datapoints.csv")
+testSet = pd.read_csv(f"{currDir}/Lab2-data-and-testpoints/testpoints.csv")
 
 # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.plot.scatter.html
 df = pd.DataFrame(trainingSet,
@@ -16,5 +18,7 @@ df.plot.scatter(
     y='height',
     c=df['label'].map({0: 'blue', 1: 'red'})
 )
+
+
 plt.show()
 
