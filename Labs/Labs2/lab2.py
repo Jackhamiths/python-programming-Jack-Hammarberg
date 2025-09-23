5a65f82844ebd85872fddb5ac3c4f60994251e1e
Jack-Hammarberg <Jackhammarberg@gmail.com>
2025-09-23 19:08:36 +0200

    Add euclidean distance calculation update test set
    updated the test set file to better match the training set and added an
    euclidean distance calculation to calculate distance between test and
    training points.

--- Labs/Labs2/lab2.py
+++ Labs/Labs2/lab2.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import numpy as np
 
 # https://www.w3schools.com/python/pandas/pandas_csv.asp 
 # https://www.geeksforgeeks.org/python/get-current-directory-python/
@@ -10,16 +11,20 @@ testSet = pd.read_csv(f"{currDir}/Lab2-data-and-testpoints/testpoints.csv")
 
 # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.plot.scatter.html
 df = pd.DataFrame(trainingSet,
-    columns=['width', 'height', 'label'])
+    columns=['width', 'height', 'pokemons'])
 
 # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.plot.scatter.html
 # pichus are blue and pikachus are red
 df.plot.scatter(
     x='width',
     y='height',
-    c=df['label'].map({0: 'blue', 1: 'red'})
+    c=df['pokemons'].map({0: 'blue', 1: 'red'})
 )
 
 
 plt.show()
+
+# https://www.geeksforgeeks.org/python/calculate-the-euclidean-distance-using-numpy/ 
+d = np.sqrt(np.sum((trainingSet - testSet)**2))
+print(d)
 
