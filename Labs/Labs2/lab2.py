9a896b0f609eac03095e6691cf0bdefe7d63e7ca
Jack-Hammarberg <Jackhammarberg@gmail.com>
2025-09-25 20:01:43 +0200

    add user input and nearest neighbor 10 algorythm
    Also added documentation on k nearest neighbor algorythm for
    clarification

--- Labs/Labs2/lab2.py
+++ Labs/Labs2/lab2.py
@@ -28,10 +28,12 @@ plt.show()
 
 
 # https://www.geeksforgeeks.org/python/calculate-the-euclidean-distance-using-numpy/ 
-d = np.sqrt(np.sum((trainingSet - testSet)**2))
+d = np.sqrt(np.sum((trainingSet - testSet)**2, axis=0))
 print(d)
 
 # https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html#sklearn.neighbors.KNeighborsClassifier
+# Prints out the class the testpoints are closest to in the datapoints-
+# as 1 for pikachu and 0 for pichu instead of yes for pichu and no for pikachu
 pokemon_dict = {1:'Pikachu',0:'Pichu'}
 test = pd.read_csv(f"{currDir}/Lab2-data-and-testpoints/testpoints.csv")
 train = pd.read_csv(f"{currDir}/Lab2-data-and-testpoints/datapoints.csv")
@@ -40,3 +42,29 @@ y = train['pokemons']
 neigh = KNeighborsClassifier(n_neighbors=1)
 neigh.fit(X, y)
 print(neigh.predict(test))
+
+try:
+    user_width = float(input("Enter width: "))
+    user_height = float(input("Enter height: "))
+    
+    if user_width < 0 or user_height < 0:
+        raise ValueError("Negative numbers are not allowed.")
+    
+    # https://stackoverflow.com/questions/69326639/sklearn-warning-valid-feature-names-in-version-1-0
+    # didn't manage to solve this myself using the fix above so i used chat gpt to get the correct syntax
+    feature_names = pd.DataFrame([[user_width, user_height]], columns=["width", "height"]) 
+
+    prediction = neigh.predict(feature_names)[0]
+    print(f"The Pokémon is likely a {pokemon_dict[prediction]}")  
+
+except ValueError as e:
+    print(f"Stop right there criminal scum you violated the law! {e}")
+
+neigh10 = KNeighborsClassifier(n_neighbors=10)
+neigh10.fit(X, y)
+predictions = neigh10.predict(test)
+print(neigh10.predict(test))
+
+
+
+
